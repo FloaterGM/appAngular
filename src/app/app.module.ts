2c17eeab956161d25e4dc44bc03009e784fcf1d6
Cristobal Valencia Hernandez <vcristobal3@gmail.com>
2022-11-12 22:53:48 -0500

    hotelAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { FormReservaComponent } from './form-reserva/form-reserva.component';
+import { HabitacionesComponent } from './habitaciones/habitaciones.component';
+import { DescripcionComponent } from './descripcion/descripcion.component';
+import { HttpClient, HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    FormReservaComponent,
+    HabitacionesComponent,
+    DescripcionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
